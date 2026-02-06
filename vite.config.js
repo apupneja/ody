fa591b14fdfd0d452cc0d15e8886e4f2c3e55959
Anirudh Pupneja <apupneja2002@gmail.com>
2026-02-05 17:38:04 -0800

    frontend init

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 const backendPort = process.env.BACKEND_PORT || 3001
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       "/api": `http://localhost:${backendPort}`,
